1558e43490bac040ab7b43f7dd722c16680e987e
chudnofskyATD <mpereannor@gmail.com>
2019-10-28 16:05:40 +0000

    getUser and getUserId tested

--- index.js
+++ index.js
@@ -2,15 +2,102 @@
 const express = require('express');
 const cors = require('cors');
 
-//const db = require( './data/');
+const db = require( './data/db');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+// app.get('/api/hubs/:id', getHubById)
+// app.get('/api/hubs', getAllHubs)
+app.post('/api/users/new', createNewUser);
+app.get('/api/users/:id', getUserById);
+app.get('/api/users', getUsers);
 app.get('*', handleDefaultRequest);
 
+// function getAllHubs(req, res) {
+//     db.find()
+//       .then(data => {
+//         console.log(data)
+//         res.json(data)
+//       })
+//       .catch(error => {
+//         console.log(error)
+//               res.status(500).json('The users information could not be retrieved.')
+
+//       })
+//   }
+function createNewUser(req, res) {
+
+    console.log(req);
+
+     const newUser = {
+        name: req.body.name,
+        bio: 'otherth',
+        created_at: 'now',
+        updated_at: 'now'
+    }
+
+    db.insert(newUser)
+}
+
+function getUserById(req, res) {
+    const { id } = req.params;
+    db.findById(id)
+    .then(data => {
+        if(data) {
+            res.status(200).json({
+                success: true,
+                data,
+            });
+        } 
+        // else if {
+        //     res.status(404).json({
+        //         success: false, 
+        //         message: 'The user with the specified ID does not exist.'
+        //     })
+        
+        else {
+            res.status(500).json({
+                success: failure, 
+                message: 'The user with the specified ID does not exist.'
+            })
+        }
+    })
+
+}
+
+function getUsers( req, res) { 
+    db.find()
+        .then(data => {
+            console.log(data);
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                err,
+            })
+        })
+}
+   
+
+// function getHubById(req, res) {
+//     const { id } = req.params;
+//     db.findById(id)
+//       .then(data => {
+//         console.log(data);
+//         res.status(200).json(data)
+//       })
+//       .error(error => {
+//         console.log(error);
+//       })
+//   }
+
+// function find
 function handleDefaultRequest(req, res) { 
     res.json('hello monde')
 }
