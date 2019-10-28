62bc08a6aa56ea13f2d3f6428072a66ac63e73ab
chudnofskyATD <mpereannor@gmail.com>
2019-10-28 13:46:57 +0000

    tested with nodemon and postman

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+
+//const db = require( './data/');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('*', handleDefaultRequest);
+
+function handleDefaultRequest(req, res) { 
+    res.json('hello monde')
+}
+app.listen(process.env.PORT || 3300, () => {
+    console.log('listening on' + (process.env.PORT || 3300));
+});
